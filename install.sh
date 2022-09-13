8b39e120e7d22481db5702cf11ee08d9b2b7b71f
moonxraccoon <moonxraccoon@proton.me>
2022-09-13 21:51:29 +0200

    changed install script

--- install.sh
+++ install.sh
@@ -30,29 +30,29 @@ case "$nvim_installed" in
         printf "[\033[32m+\033[0m] Installing Neovim prerequisites..."
         # Install neovim prerequisites for building
         sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen build-essential > /dev/null
-        printf "[\033[32mOk!\033[0m\n"
+        printf "[\033[32mOk\033[0m]\n"
         printf "[\033[32m+\033[0m] Compiling latest Neovim..."
         # Clone neovim repo
         git clone https://github.com/neovim/neovim /home/$USER/Downloads/neovim/ > /dev/null
         cd ~/Downloads/neovim/
         make CMAKE_BUILD_TYPE=RelWithDebInfo -j$(nproc) > /dev/null
-        printf "[\033[32mOk!\033[0m\n"
+        printf "[\033[32mOk\033[0m]\n"
         printf "[\033[32m+\033[0m] Installing..."
         sudo make install -j$(nproc)
-        printf "[\033[32mOk!\033[0m\n"
+        printf "[\033[32mOk!\033[0m]\n"
         ;;
 esac
 
 # # Install packer.nvim as a plugin manager for neovim
 printf "[\033[32m+\033[0m] Installing Plugin Manager..."
-printf "[\033[32mOk!\033[0m\n"
+printf "[\033[32mOk!\033[0m]\n"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim > /dev/null 2>&1
 # 
 cp -r $cwd/nvim /home/$USER/.config/
 
 printf "[\033[32m+\033[0m] Installing Neovim plugins..."
 nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync" > /dev/null 2>&1
-printf "[\033[32mOk!\033[0m\n"
+printf "[\033[32mOk!\033[0m]\n"
 
 printf "\n\n\033[32mConfig files written into:\n"
 printf " ==> \033[31m~/.config/nvim\n\n"
