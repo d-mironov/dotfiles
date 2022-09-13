971057246ced8d135111f705b1b7ceb69caba4bf
moonxraccoon <moonxraccoon@proton.me>
2022-09-13 22:08:49 +0200

    added experimental install script

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ printf "     _       _    __ _ _           \n"
 sleep 0.1
 printf "    | |     | |  / _(_) |          \n"
 sleep 0.1
-printf "  __| | ___ | |_| |_ _| | ___  ___ \n"
-sleep 0.1
+printf "  __| | ___ | |_| |_ _| | ___  ___ \n" sleep 0.1
 printf " / _  |/ _ \\| __|  _| | |/ _ \\/ __|\n"
 sleep 0.1
 printf "| (_| | (_) | |_| | | | |  __/\\__ \ \n"
@@ -21,7 +20,7 @@ printf "\033[0m"
                                    
 
 cwd=$(pwd)
-printf "[\033[32m?\033[0m] Do you have \033[32mnvim[>=v0.7.0]\033[0m installed? [y/n] > "
+printf "[\033[32m?\033[0m] Do you have \033[32mnvim[>=v0.7.0]\033[0m installed? [y/N] > "
 read -r nvim_installed
 case "$nvim_installed" in
     [yY][eE][sS]|[yY])
@@ -31,12 +30,12 @@ case "$nvim_installed" in
         # Install neovim prerequisites for building
         sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen build-essential > /dev/null
         printf "\033[32mOk\033[0m\n"
-        printf "[\033[32m+\033[0m] Compiling latest Neovim..."
+        printf "[\033[32m+\033[0m] Compiling latest Neovim...\n"
         # Clone neovim repo
-        $(git clone https://github.com/neovim/neovim /home/$USER/Downloads/neovim/) > /dev/null
+        git clone https://github.com/neovim/neovim /home/$USER/Downloads/neovim/
         cd ~/Downloads/neovim/
         make CMAKE_BUILD_TYPE=RelWithDebInfo -j$(nproc) > /dev/null 2>1&
-        printf "\033[32mOk\033[0m\n"
+        printf "[\033[32m+]\033[0m]\033[32mDone\033[0m\n"
         printf "[\033[32m+\033[0m] Installing..."
         sudo make install -j$(nproc) > /dev/null  2>1&
         printf "\033[32mOk\033[0m\n"
@@ -66,4 +65,14 @@ printf '      "colorscheme gruvbox-material\n'
 printf '      "colorscheme onedarkpro\n'
 printf "  \033[32m]]\n\033[0m"
 
-
+printf "[\033[32m?\033[0m] Do you want to install kitty as your terminal? [y/N] > "
+read -r install_kitty
+case "$nvim_installed" in
+    [yY][eE][sS]|[yY])
+        printf "[\033[32m+\033[0m] Installing kitty..."
+        sudo apt install kitty > /dev/null
+        printf "\033[32mOk\033[0m\n"
+        ;;
+    *)
+        ;;
+esac
