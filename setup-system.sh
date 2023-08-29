27b7ac71763fd517960ae6f101e86f717bfdec68
Daniel Mironov <mail@danielmironov.dev>
2023-08-30 01:47:50 +0200

    added general idea for easier system setup script

---
+++ setup-system.sh
@@ -0,0 +1,6 @@
+# 1. Upgrade the system (sudo apt upgrade)
+# 2. Install all necessary tools
+# - wget, curl, git, fish, exa, kitty, nala
+# 3. Import ssh keys
+# 4. Get dotfiles and execute install.sh script
+# 5. 
