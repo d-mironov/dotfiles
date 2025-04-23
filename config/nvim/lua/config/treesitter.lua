4929b58e8f4de120b02d59824ced60e8b050efb2
Daniel Mironov <daniel.mironow@emi.fraunhofer.de>
2025-04-23 11:18:12 +0200

    added only necessary parsers for treesitter

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,25 @@
 local treesitter = require("nvim-treesitter.configs")
 treesitter.setup {
-    ensure_installed = "all",
+    ensure_installed = { 
+        "c", "cpp", "zig", "asm", "devicetree",
+        "verilog", "vhdl", 
+        "python", -- "pip_requirements",
+        "rust",
+        "yaml", "toml", "csv", "json", "json5", "xml",
+        "go", "gomod",
+        "julia", 
+        "diff", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
+        "make", "cmake", "cairo", "kconfig", "linkerscript", "ninja", "objdump",
+        "lua", "vim", "vimdoc", "comment", "meson", "regex", 
+        "markdown", "markdown_inline",
+        "css", "scss", "html", "typescript", "tsx", "http", "htmldjango", "javascript", "jsdoc",
+        "php", "phpdoc", "php_only", "sql", "svelte", "vue", 
+        "java", "properties", 
+        "udev", "bash", "fish", "desktop", "dockerfile", "nix", "passwd", "printf", "ssh_config", "tmux",
+        "xresources", "xcompose", 
+        "typst", "latex", "bibtex",
+        "c_sharp",
+    },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
